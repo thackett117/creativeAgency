2c993b9d358926d7ba5554a39a0b5e619eff93b5
Terrence Hackett <thackett117@gmail.com>
2021-07-12 22:41:03 -0500

    made example title a link back to home page

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 const toggle = document.querySelector(".toggle");
 const navigation = document.querySelector(".navigation");
+const homeTitle = document.querySelector(".logo");
 
 toggle.addEventListener("click", () => {
   toggle.classList.toggle("active");
   navigation.classList.toggle("active");
 });
+
+homeTitle.addEventListener("click", () => {
+  location.href = "index.html"
+})
